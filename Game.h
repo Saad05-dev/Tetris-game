5278d29d00f1b249f837802ef24a33a477108ac7
Saad05-dev <bougrasaad@gmail.com>
2025-04-05 17:51:01 +0000

    Game class

---
+++ Game.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+
+class Game
+{
+private:
+    
+    //Variables
+    //Window
+    sf::RenderWindow *window;
+    sf::VideoMode videoMode;
+    sf::Event ev;
+    //Game Objects
+
+    //resources
+
+    //Text
+
+    //Game Logic
+
+    //Private functions
+public:
+    //Costructors / Deconstructors
+    Game();
+    virtual ~Game();
+    //accessors
+
+    //Functions
+
+};
+
