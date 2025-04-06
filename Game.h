4b899eaab25cb8768613056e09d3abb736b5a3e8
Saad05-dev <bougrasaad@gmail.com>
2025-04-06 19:24:51 +0100

    Game class

--- Game.h
+++ Game.h
@@ -22,15 +22,29 @@ private:
     //Text
 
     //Game Logic
+    bool endGame;
+    unsigned points;
+    float blocksSpawnTimer;
+    float blocksSpawnTimerMax;
 
+    //Game objects
+    
     //Private functions
+    void initVariables();
+    void initWindow();
 public:
     //Costructors / Deconstructors
     Game();
     virtual ~Game();
+
     //accessors
+    const bool running() const;
+    const bool getEndGame() const;
 
     //Functions
-
+    void pollEvents();
+    void update();
+    
+    void render();
 };
 
